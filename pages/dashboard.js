e3b792e6407fbbd132e3cf60894f3b3daa793650
Nathaniel Saludes <saludesnathaniel@gmail.com>
2021-01-08 16:00:46 +0800

    replacaed loading state with a condition rendering of EmptyState component or FreePlan EmptyState

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,22 +1,19 @@
 import Head from 'next/head';
 import { useAuth } from "@/lib/auth";
 import EmptyState from "@/components/EmptyState";
+import FreePlanEmptyState from '@/components/FreePlanEmptyState';
 
 import { Button } from "@chakra-ui/react";
 
 export default function Dashboard() {
 	const auth = useAuth();
 
-	if (!auth.user) {
-		return 'Loading...'
-	}
-
 	return (
 		<>
 			<Head>
 				<title>Dashboard</title>
 			</Head>
-			<EmptyState />
+			{auth.user ? <EmptyState /> : <FreePlanEmptyState />}
 		</>
 	)
 }
